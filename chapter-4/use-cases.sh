4c4053e299c24478668a59cf4e6036608afa0071
Sergei <sleepytomcat@gmail.com>
2018-03-26 23:37:42 -0700

    Gradle wrapper

---
+++ chapter-4/use-cases.sh
@@ -0,0 +1,10 @@
+# list of sub-projects for current project
+gradle projects
+
+# sub-project tasks
+gradle :sub-project-1:tasks
+
+# generate Gradle wrapper
+gradle wrapper
+# ./gradle/wrapper/* files, as well as ./gradlew, ./gradlew.bat should be under version control,
+# as that allows to build a project with ./gradlew even if Gradle has never been installed on the system.
