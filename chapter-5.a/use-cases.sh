ce7ec9abd63bb7dd90d8cb163732981eeeb7637c
Sergei <sleepytomcat@gmail.com>
2018-03-31 14:22:48 -0700

    Custom task classes

---
+++ chapter-5.a/use-cases.sh
@@ -0,0 +1,4 @@
+# note that tasks with group parameter assigned are displayed in a dedicated section
+gradle tasks --all
+# let's run the task; note the dependency
+gradle showDate 
---
+++ chapter-5.b/use-cases.sh
@@ -0,0 +1,4 @@
+# note that tasks with group parameter assigned are displayed in a dedicated section
+gradle tasks --all
+# let's run the task we've declared and defined
+gradle showDateTime 
